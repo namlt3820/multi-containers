463e385a825320222baae89bad7afa889694874a
Lê Trung Nam <namlt3820@gmail.com>
2021-01-15 16:37:17 -0800

    add Dockerrun.aws.json

--- server/index.js
+++ server/index.js
@@ -53,7 +53,19 @@ app.get('/values/all', async (req, res) => {
   res.send(values.rows);
 });
 
+app.get('/api/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
 app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.get('/api/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
