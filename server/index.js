90ef3b27951a6109033530f1f2cb5e713fca59fc
Lê Trung Nam <namlt3820@gmail.com>
2021-01-15 16:42:54 -0800

    fix server service

--- server/index.js
+++ server/index.js
@@ -49,23 +49,10 @@ app.get('/', (req, res) => {
 
 app.get('/values/all', async (req, res) => {
   const values = await pgClient.query('SELECT * from values');
-
-  res.send(values.rows);
-});
-
-app.get('/api/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
-
   res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
-});
-
-app.get('/api/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
   });
